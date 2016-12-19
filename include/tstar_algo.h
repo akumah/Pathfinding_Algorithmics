9466f1fc473eb6e7bd6a385128348e370658b83f
Hendrik Ehrpais <Hendrik Ehrpais>
2016-12-19 18:49:36 +0200

    Backend for Tstar in progress

--- include/tstar_algo.h
+++ include/tstar_algo.h
@@ -1,3 +1,70 @@
+#include <iostream> 
+#include <cstdlib>  
+#include <cmath> 
+using namespace std;
+class vertex_s{
+	
+	vertex_s* parent;
+	int gvalue;
+	
+	public:
+	int x; int y;
+	vertex_s(int, int);
+	
+	vertex_s getParent();
+	void setParent(vertex_s* par);
+	
+	int get_gvalue();
+	void set_gvalue(int gval);
+	
+	double straight_distance(vertex_s othervertex);
+};
+
+vertex_s vertex_s::getParent(){
+	return *parent;
+}
+
+void vertex_s::setParent(vertex_s* par){
+	parent = par;
+}
+vertex_s::vertex_s(int x_init, int y_init){
+	x = x_init;
+	y = y_init;
+}
+int vertex_s::get_gvalue(){
+	return gvalue;
+}
+void vertex_s::set_gvalue(int gval){
+	gvalue = gval;
+}
+
+double vertex_s::straight_distance(vertex_s othervertex){
+	int d1; int d2;
+	double distance;
+	if (othervertex.x >= x) d1 = othervertex.x - x;
+	else d1 = x - othervertex.x;
+	
+	if (othervertex.y >= y) d2 = othervertex.y - y;
+	else d2 = y - othervertex.y;
+	
+	distance= sqrt(d1*d1 + d2*d2);
+	return distance;
+}
+//[column1[row1,row2... rowN], column2[row1,row2... rowN]...columnN[row1,row2... rowN]], where column is y and row is x is the current format of the grid
+class grid{
+	public:
+	bool[][] gridcoordinates;
+	grid(bool[][]);
+	
+	bool isBlocked(int x, int y);
+};
+
+grid::grid(bool[][] coords){
+	gridcoordinates = coords;
+}
+
+grid::isBlocked(int x, int y)
+
 
 void tstar_UpdateVertex (int *arrays, int arraylength);
 void tstar_ComputeCost (int *arrays, int arraylength);
--- src/tstar_algo.cpp
+++ src/tstar_algo.cpp
@@ -1,11 +1,52 @@
 #include <iostream> /* Sisend ja väljund */
 #include <cstdlib>  /* EXIT_SUCCESS jt konstandid */
 #include "tstar_algo.h"
-  
+#include <vector>  
 using namespace std; /* Nimeruumi valik (selgitatakse praktikumis) */
+
+
 
 void tstar_UpdateVertex (int *arrays, int arraylength){
 }
 
 void tstar_ComputeCost (int *arrays, int arraylength){
+}
+void tstar_mainLoop (int *arrays, int arraylength){
+}
+void tstar_lineofsight(vertex_s s0, vertex_s s1, grid xy){
+	int x0; int y0; int x1; int y1; int dx; int dy; int sx; int sy; int f;
+	
+	x0 = s0.x;
+	y0 = s0.y;
+	
+	x1 = s1.x
+	y1 = s1.y
+	
+	dy = y1-y0;
+	dx = x1-x0; 
+	
+	f = 0;
+	
+	if (dy < 0){
+		dy = -dy;
+		sy = -1;
+	}
+	else sy = 1;
+	
+	
+	if (dx < 0){
+		dx = -dx;
+		sx = -1;
+	}
+	else sx = 1;
+	
+	if (dx >= dy){
+		while (x0 != x1){
+			f = f+ dy;			
+			if (f >= dx){
+				
+			}
+		}
+	}
+		
 }
